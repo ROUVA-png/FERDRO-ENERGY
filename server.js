4f8c9c9b719656ba5dafb81f41a0d9c798e189c8
ROUVA-png <onoskelvin100@gmail.com>
2025-02-13 11:14:28 -0800

    Update server.js

--- server.js
+++ server.js
@@ -137,7 +137,7 @@ app.delete("/users/:id", async (req, res) => {
 });
 
 // Start Server
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0" () => {
     console.log(`Server running on port ${PORT}`);
 });
 
